edd405db5954af8b74adcd696fdd9fca4929b82b
Miguel Rovlich Bastias <72398403+miguelRovlich@users.noreply.github.com>
2022-04-05 17:37:19 -0400

    Add files via upload
    subir proyecto a github pages

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: [
+    'vuetify'
+  ],
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/portfolio/'
+  : '/'
+})
